4baa7bb8220b3a190f6b46fb0c058036b51253ff
Bilka <Bilka2@users.noreply.github.com>
2018-05-11 21:50:09 +0200

    Added command to run wiki scripts that are in python

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import discord
 import json
+import base64
+import sys
+sys.path.append('C:\\Users\\Erik\\Documents\\GitHub\\Wiki-scripts')
+from analytics import main as wiki_analytics
+from new_fff import main as wiki_new_fff
 
 with open('config.json', 'r') as f:
   config = json.load(f)
@@ -7,7 +12,7 @@ with open('config.json', 'r') as f:
 if config['token'] == '':
   print('No token.')
 
-TOKEN = config['token']
+TOKEN = base64.b64decode(config['token']).decode('utf-8')
 
 client = discord.Client()
 
@@ -19,6 +24,9 @@ async def on_message(message):
   
   if message.content.startswith('!hello'):
     msg = 'Hello {0.author.mention}'.format(message)
+    await client.send_message(message.channel, msg)
+  if message.content.startswith('!friday'):
+    msg = wiki_analytics() + '\n' + wiki_new_fff()
     await client.send_message(message.channel, msg)
 
 @client.event
