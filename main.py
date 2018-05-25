27bc1070e8cee8bb9278244f3c17f0e1496ba86b
Bilka <Bilka2@users.noreply.github.com>
2018-05-25 21:51:05 +0200

    Run Friday actions automatically

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import discord
-import json
+import asyncio
 import base64
+import discord
+import feedparser
+import json
 import sys
 
 with open('config.json', 'r') as f:
@@ -10,12 +12,26 @@ sys.path.append(config['path_to_wiki_scripts'])
 from analytics import main as wiki_analytics
 from new_fff import main as wiki_new_fff
 
-if config['token'] == '':
-  print('No token.')
-
 TOKEN = base64.b64decode(config['token']).decode('utf-8')
-
 client = discord.Client()
+
+with open('feeds.json', 'r') as f:
+  feeds = json.load(f)
+
+async def update_feeds():
+  await client.wait_until_ready()
+  while not client.is_closed:
+    for name, entry in feeds.items():
+      feed = feedparser.parse(entry['url'])
+      if feed.entries[0].updated > entry['time_latest_entry'] and name == 'fff':
+        msg = 'Ran wiki script:\n' + wiki_analytics() + '\n' + wiki_new_fff()
+        channel = client.get_channel(entry['channel'])
+        print(msg)
+        await client.send_message(channel, msg)
+        feeds[name]['time_latest_entry'] = feed.entries[0].updated
+        with open('feeds.json', 'w') as f:
+          json.dump(feeds, f)
+    await asyncio.sleep(300)
 
 @client.event
 async def on_message(message):
@@ -33,7 +49,14 @@ async def on_message(message):
 async def on_ready():
   print('Logged in as')
   print(client.user.name)
-  print(client.user.id)
   print('------')
 
-client.run(TOKEN)
+loop = asyncio.get_event_loop()
+try:
+  task = loop.create_task(update_feeds())
+  loop.run_until_complete(client.start(TOKEN))
+except KeyboardInterrupt:
+  task.cancel()
+  loop.run_until_complete(client.logout())
+finally:
+  loop.close()
