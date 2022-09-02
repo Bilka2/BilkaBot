d1d8364b07665f21b3ca2ee92f760669ab3638b1
Bilka <Bilka2@users.noreply.github.com>
2022-09-02 21:42:25 +0200

    Line endings

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # run from home directory
-source wiki_bot_env/bin/activate; cd BilkaBot/; python3 main.py; deactivate
+source wiki_bot_env/bin/activate; cd BilkaBot/; python3 main.py; deactivate;
