95a3cf702a799aaa70d6c19bba45170b95904689
Bilka <Bilka2@users.noreply.github.com>
2022-09-02 22:07:01 +0200

    How did this ever work?

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 # run from home directory
+# may need to set this to be executable: chmod +x start.sh
 source wiki_bot_env/bin/activate; cd BilkaBot/; python3 main.py; deactivate;
